d552a9d534a6c7a1d13eb0a59849b462b30ffa65
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2024-04-13 17:26:37 -0400

    finish bubblesort in python

--- Sesion_didactica_1/BubbleSortInPython.py
+++ Sesion_didactica_1/BubbleSortInPython.py
@@ -1,8 +1,9 @@
 # Miguel Figuera Quintero V-23.558.789 Seccion 8B
 
 def bubble_sort(array:list):
+    print(array)
     #Numero de iteraciones posibles.
-    num=len(array)
+    num=len(array)-1
     #Loop de primer nivel para la cantidad de iteraciones
     for x in range(num):
         #variable para determinar break-case
@@ -10,7 +11,9 @@ def bubble_sort(array:list):
         #loop de segundo nivel (nested loop) para cada elemento del array
         for index,element in enumerate(array):
             #comparar elementos y ver si el actual es menor que el anterior
-            if element < array[index-1]:
+            #Eliminando la posibilidad de que el array se muerda la cola puesto
+            #que en python array[-1] es igual al ultimo dato del array
+            if element < array[index-1] and index != 0:
                 #intercambiar elementos con ayuda de un auixiliar(helper)
                 #para no perder la informacion
                 helper=array[index-1]
@@ -20,9 +23,13 @@ def bubble_sort(array:list):
                 array[index]=helper
                 #determinar que no se cumple el break-case
                 swapped=True
+                #print('array:',array)
         #break-case
         if not swapped:
             break
     #retornar el array ordenado
     return array
 # Fin del proceso marcado por indentacion.
+
+
+print(bubble_sort([9,8,7,6,5,4,3,2,1]))
