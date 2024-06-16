e98ccc097c527fed34d3cedc33f367375c7bdd61
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2024-06-16 13:06:00 -0400

    done

--- sesion_didactica_3/flask_crud/App.py
+++ sesion_didactica_3/flask_crud/App.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 #inicializar Flask App.
@@ -11,8 +11,7 @@ app.secret_key='RackhamCalicoJack'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
 db=SQLAlchemy(app)
 
-#clase para guardar datos y modelar datos:
-
+#clase para modelar datos:
 class Task(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(200))
@@ -21,6 +20,7 @@ class Task(db.Model):
     create_at=db.Column(db.DateTime, default=db.func.current_timestamp())
     
 
+#Creacion de la base de datos con los modelos correspondientes.
 with app.app_context():
     db.create_all()
 
@@ -34,13 +34,41 @@ def index():
     return render_template('index.html',list_tasks=tasks)
 
 
-
+#define la ruta para crear tareas
 @app.route('/create-task',methods=['POST'])
+#define la funcion para crear tareas
 def create_task():
     new_task=Task(title=request.form['task_title'],description=request.form['task_description'],complete=False,create_at=db.func.current_timestamp())
     db.session.add(new_task)
     db.session.commit()
-    return index()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete_task(id):
+    Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<id>')
+def update_task(id):
+    task=Task.query.filter_by(id=int(id)).first()
+    task.complete=True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/<id>')
+def edit_page(id):
+    task=Task.query.filter_by(id=int(id)).first()
+    return render_template('edit.html',task=task)
+
+@app.route('/edit-task/<id>',methods=['POST'])
+def edit_task(id):
+    task=Task.query.filter_by(id=int(id)).first()
+    task.title=request.form['task_title_edit']
+    task.description=request.form['task_description_edit']
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 #Debug activado.
